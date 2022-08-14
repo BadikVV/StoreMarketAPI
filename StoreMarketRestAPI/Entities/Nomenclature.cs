629b2e52e8c314255d67bdb64c701b9593282946
Vlad <vladis.osminin@gmail.com>
2022-08-14 14:31:41 +0330

    Add initial project files

--- StoreMarketRestAPI/Entities/Nomenclature.cs
+++ StoreMarketRestAPI/Entities/Nomenclature.cs
@@ -3,5 +3,9 @@ namespace StoreMarketRestAPI.Entities;
 
 public class Nomenclature
 {
-    public List<Product> Products { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    private string MeasureUnit { get; set; }
+    public Manufacture Manufacture { get; set; }
+    public int Quantity { get; set; }
 }
