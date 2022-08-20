a22589989ca8f7c00f3e6c68f737b0cbd5320161
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:09:33 +0330

    Add "builders"

--- StoreMarketRestAPI/Program.cs
+++ StoreMarketRestAPI/Program.cs
@@ -1,15 +1,17 @@
-using StoreMarketRestAPI.Infrastructure.GenericRepository;
+using System.Reflection;
 using StoreMarketRestAPI.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using StoreMarketRestAPI.Entities;
 using StoreMarketRestAPI.Infrastructure.Context;
-
+using StoreMarketRestAPI.Infrastructure.Generics;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataDbContext>(options => 
     options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MarketStore;Trusted_Connection=True;"));
-builder.Services.AddTransient<IGenericRepository<Nomenclature>, GenericRepository<Nomenclature>>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddTransient<IGenericRepository, GenericRepository>();
+builder.Services.AddTransient<GenericService>();
 
 builder.Services.AddCors(options =>
 {
