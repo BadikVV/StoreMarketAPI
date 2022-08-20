5168e1b36b6aa0516d96af476d9cb436be60bf30
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:06:29 +0330

    Add ProductMapping

---
+++ StoreMarketRestAPI/Mappings/ProductMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using StoreMarketRestAPI.DTOs;
+using StoreMarketRestAPI.Entities;
+
+namespace StoreMarketRestAPI.Mappings;
+
+public class ProductMapping : Profile
+{
+    public ProductMapping()
+    {
+        CreateMap<ProductCreateDTO, Product>().ReverseMap();
+        CreateMap<ProductUpdateDTO, Product>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
