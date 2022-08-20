7df8cd9e01255209321c309b4cd526ae52385c77
Vlad <vladis.osminin@gmail.com>
2022-08-20 19:57:22 +0330

    Add a DbSet functions

--- StoreMarketRestAPI/Infrastructure/Context/DataDbContext.cs
+++ StoreMarketRestAPI/Infrastructure/Context/DataDbContext.cs
@@ -9,7 +9,11 @@ public sealed class DataDbContext : DbContext
         
     }
     public DbSet<Nomenclature> Nomenclatures { get; set; }
-    
+    public DbSet<Manufacturer> Manufacturers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Warehouse> Warehouses { get; set; }
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
