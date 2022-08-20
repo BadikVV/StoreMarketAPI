39109187b90136a25d749175a3b5501ce2861359
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:05:53 +0330

    Add OrderMapping

---
+++ StoreMarketRestAPI/Mappings/OrderMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using StoreMarketRestAPI.DTOs;
+using StoreMarketRestAPI.Entities;
+
+namespace StoreMarketRestAPI.Mappings;
+
+public class OrderMapping : Profile
+{
+    public OrderMapping()
+    {
+        CreateMap<OrderCreateDTO, Order>().ReverseMap();
+        CreateMap<OrderUpdateDTO, Order>().ReverseMap();
+        CreateMap<Order, OrderDTO>().ReverseMap();
+    }
+}
