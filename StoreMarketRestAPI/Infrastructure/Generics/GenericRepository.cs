834af2608b6392015fff84396259b413a6948162
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:19:02 +0330

    Changed the methods of the GenericRepository

--- StoreMarketRestAPI/Infrastructure/Generics/GenericRepository.cs
+++ StoreMarketRestAPI/Infrastructure/Generics/GenericRepository.cs
@@ -1,35 +1,37 @@
 ﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using StoreMarketRestAPI.Base;
 using StoreMarketRestAPI.Infrastructure.Context;
 using StoreMarketRestAPI.Interfaces;
 
-namespace StoreMarketRestAPI.Infrastructure.GenericRepository;
+namespace StoreMarketRestAPI.Infrastructure.Generics;
 
-public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity: class
+public class GenericRepository : IGenericRepository
 {
     private readonly DataDbContext _context;
-    private readonly DbSet<TEntity> _dbSet;
+    //private readonly DbSet<TEntity> _dbSet;
 
     public GenericRepository(DataDbContext context)
     {
         _context = context;
-        _dbSet = context.Set<TEntity>();
+        //_dbSet = context.Set<TEntity>();
     }
 
-    public List<TEntity> GetList(int pageSize, int pageNumber, string? orderDirection = "asc", 
-        Expression<Func<TEntity, object>>? orderExpression = null, Expression<Func<TEntity, bool>> ? expression = null)
+    public List<TEntity> GetList<TEntity>(int pageSize, int pageNumber, string? orderDirection = "asc", 
+        Expression<Func<TEntity, object>>? orderExpression = null, Expression<Func<TEntity, bool>> ? expression = null) where TEntity : BaseEntity
     {
-        IQueryable<TEntity>? query = null;
+        IQueryable<TEntity>? query = _context.Set<TEntity>();;
         
         if (orderExpression == null)
         {
-            query = _dbSet.OrderBy(b => b);
+            query = _context.Set<TEntity>().OrderBy(b => b);
         }
         else
         {
             if (orderDirection != "asc" && orderDirection != "desc")
                 orderDirection = "asc";
-            query = orderDirection == "asc" ? _dbSet.OrderBy(orderExpression) : _dbSet.OrderByDescending(orderExpression);
+            query = orderDirection == "asc" ? _context.Set<TEntity>().OrderBy(orderExpression) : 
+                _context.Set<TEntity>().OrderByDescending(orderExpression);
         }
         if (expression != null)
             query = query.Where(expression);
@@ -39,30 +41,38 @@ public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEnt
             .ToList();
     }
 
-    public TEntity GetById(object id)
+    public TEntity GetById<TEntity>(Guid id) where TEntity : BaseEntity
     {
-        return _dbSet.Find(id)!;
+        return _context.Set<TEntity>().Find(id)!;
     }
 
-    public void Add(TEntity entity)
+    public TEntity Add<TEntity>(TEntity entity) where TEntity : BaseEntity
     {
-        _dbSet.Add(entity);
+        _context.Set<TEntity>().Add(entity);
+        _context.Entry(entity).State = EntityState.Added;
         _context.SaveChanges();
+        return entity;
     }
 
-    public void Update(TEntity entity)
+    public void Update<TEntity>(TEntity entity) where TEntity : BaseEntity
     {
         // var newEntity = GetById(id);
-        _dbSet.Attach(entity);
+        _context.Set<TEntity>().Attach(entity);
         _context.Entry(entity).State = EntityState.Modified;
         _context.SaveChanges();
         
     }
 
-    public void Delete(object id)
+    public void Delete<TEntity>(Guid id) where TEntity : BaseEntity
     {
-        var entity = _dbSet.Find(id);
-        Delete(entity!);
+        var entity = _context.Set<TEntity>().Find(id);
+        if(entity == null) return;
+        
+        if (_context.Entry(entity).State == EntityState.Detached)
+        {
+            _context.Set<TEntity>().Attach(entity);
+        }
+        _context.Set<TEntity>().Remove(entity);
         _context.SaveChanges();
     }
     
