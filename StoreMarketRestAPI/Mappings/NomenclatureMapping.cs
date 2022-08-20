6589283b8e9c71e321474f41ece83bb6ff1aed8c
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:05:36 +0330

    Add NomenclatureMapping

---
+++ StoreMarketRestAPI/Mappings/NomenclatureMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using StoreMarketRestAPI.Entities;
+using StoreMarketRestAPI.DTOs;
+
+namespace StoreMarketRestAPI.Mappings;
+
+public class NomenclatureMapping : Profile
+{
+    public NomenclatureMapping()
+    {
+        CreateMap<NomenclatureCreateDTO, Nomenclature>().ReverseMap();
+        CreateMap<NomenclatureUpdateDTO, Nomenclature>().ReverseMap();
+        CreateMap<Nomenclature, NomenclatureDTO>().ReverseMap();
+    }
+}
