30e678456c09533808b6b102d2da7fbefe9c16fa
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:07:11 +0330

    Add WarehouseMApping

---
+++ StoreMarketRestAPI/Mappings/WarehouseMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using StoreMarketRestAPI.DTOs;
+using StoreMarketRestAPI.Entities;
+
+namespace StoreMarketRestAPI.Mappings;
+
+public class WarehouseMapping : Profile
+{
+    public WarehouseMapping()
+    {
+        CreateMap<WarehouseCreateDTO, Warehouse>().ReverseMap();
+        CreateMap<WarehouseUpdateDTO, Warehouse>().ReverseMap();
+        CreateMap<Warehouse, WarehouseDTO>().ReverseMap();
+    }
+}
