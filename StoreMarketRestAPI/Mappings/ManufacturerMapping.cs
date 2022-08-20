a539d565639ea6ab08a1d978de59d9b6c2d5e6ba
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:04:07 +0330

    Add ManufactureMapping

---
+++ StoreMarketRestAPI/Mappings/ManufacturerMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using StoreMarketRestAPI.Entities;
+using StoreMarketRestAPI.DTOs;
+
+namespace StoreMarketRestAPI.Mappings;
+
+public class ManufacturerMapping : Profile
+{
+    public ManufacturerMapping()
+    {
+        CreateMap<ManufacturerCreateDTO, Manufacturer>().ReverseMap();
+        CreateMap<ManufacturerUpdateDTO, Manufacturer>().ReverseMap();
+        CreateMap<Manufacturer, ManufacturerDTO>().ReverseMap();
+        CreateMap<ManufacturerBaseDTO, Manufacturer>().ReverseMap();
+    }
+}
