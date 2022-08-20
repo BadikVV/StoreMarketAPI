7ad6e46328c2d84755838d4084170dbefbe66528
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:10:47 +0330

    Delete strings

--- StoreMarketRestAPI.DTOs/OrderBaseDTO.cs
+++ StoreMarketRestAPI.DTOs/OrderBaseDTO.cs
@@ -4,6 +4,4 @@ public class OrderBaseDTO
 {
     public OrderStatus Status { get; set; }
     public List<ProductDTO> Products { get; set; }
-    public DateTime RegistrationTime { get; set; }
-    public DateTime ReceptionTime { get; set; }
 }
