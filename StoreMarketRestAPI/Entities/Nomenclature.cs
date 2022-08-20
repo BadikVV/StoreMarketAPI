4157e270c1cb66b9e111bcc1d8cb920eb1d9485c
Vlad <vladis.osminin@gmail.com>
2022-08-20 20:15:47 +0330

    Delete strings

--- StoreMarketRestAPI/Entities/Nomenclature.cs
+++ StoreMarketRestAPI/Entities/Nomenclature.cs
@@ -1,12 +1,11 @@
-﻿
+﻿using StoreMarketRestAPI.Base;
 namespace StoreMarketRestAPI.Entities;
 
-public class Nomenclature
+public class Nomenclature : BaseEntity
 {
     public string Name { get; set; }
     public string Category { get; set; }
     private string MeasureUnit { get; set; }
     public Manufacturer Manufacturer { get; set; }
     public int Quantity { get; set; }
-    public Guid Id { get; set; }
 }
